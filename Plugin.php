ca9a805606739c84c67a8c5b455dc71702561887
Chatura Dilan Perera <dilan@dilan.me>
2026-02-24 19:41:44 +0530

    Strip to CLI-only: remove Server/DeployLog models, controllers, migrations, nav

--- Plugin.php
+++ Plugin.php
@@ -2,7 +2,6 @@
 
 namespace Dilexus\Deploy;
 
-use Backend;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
@@ -10,58 +9,16 @@ class Plugin extends PluginBase
     public function pluginDetails(): array
     {
         return [
-            'name' => 'dilexus.deploy::lang.plugin.name',
-            'description' => 'dilexus.deploy::lang.plugin.description',
-            'author' => 'Dilexus',
-            'icon' => 'icon-rocket',
+            'name'        => 'Dilexus Deploy Commander',
+            'description' => 'CLI commands to deploy plugins to remote servers via RainLab Deploy.',
+            'author'      => 'Dilexus',
+            'icon'        => 'icon-rocket',
         ];
     }
 
-    public function register()
+    public function register(): void
     {
         $this->registerConsoleCommand('deploy.list', \Dilexus\Deploy\Console\ListServers::class);
         $this->registerConsoleCommand('deploy.run', \Dilexus\Deploy\Console\DeployServer::class);
-    }
-
-    public function registerNavigation(): array
-    {
-        return [
-            'deploy' => [
-                'label' => 'dilexus.deploy::lang.nav.deploy',
-                'url' => Backend::url('dilexus/deploy/servers'),
-                'icon' => 'icon-rocket',
-                'permissions' => ['dilexus.deploy.*'],
-                'order' => 500,
-
-                'sideMenu' => [
-                    'servers' => [
-                        'label' => 'dilexus.deploy::lang.nav.servers',
-                        'icon' => 'icon-server',
-                        'url' => Backend::url('dilexus/deploy/servers'),
-                        'permissions' => ['dilexus.deploy.servers'],
-                    ],
-                    'deploylogs' => [
-                        'label' => 'dilexus.deploy::lang.nav.deploy_logs',
-                        'icon' => 'icon-list-alt',
-                        'url' => Backend::url('dilexus/deploy/deploylogs'),
-                        'permissions' => ['dilexus.deploy.logs'],
-                    ],
-                ],
-            ],
-        ];
-    }
-
-    public function registerPermissions(): array
-    {
-        return [
-            'dilexus.deploy.servers' => [
-                'label' => 'dilexus.deploy::lang.permissions.manage_servers',
-                'tab' => 'dilexus.deploy::lang.plugin.name',
-            ],
-            'dilexus.deploy.logs' => [
-                'label' => 'dilexus.deploy::lang.permissions.view_logs',
-                'tab' => 'dilexus.deploy::lang.plugin.name',
-            ],
-        ];
     }
 }
